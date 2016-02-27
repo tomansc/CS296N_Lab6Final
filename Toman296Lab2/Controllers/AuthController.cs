99933cdb45d716da9354a48120828544a5d49d8b
tomansc <samantha.toman@gmail.com>
2016-02-26 20:44:39 -0800

    Final version of Lab 6.

---
+++ Toman296Lab2/Controllers/AuthController.cs
@@ -0,0 +1,144 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Toman296Lab6.Models;
+using System.Web.Http;
+
+namespace Toman296Lab6.Controllers
+{
+    public class AuthController : Controller
+    {
+        UserManager<Member> userManager = new UserManager<Member>(new UserStore<Member>(new Toman296Lab2Context()));
+
+
+        /*public AuthController()
+            : this(Startup.UserManagerFactory.Invoke())
+        {
+        }
+
+        public AuthController(UserManager<AppUser> userManager)
+        {
+            this.userManager = userManager;
+        }*/
+        [AllowAnonymous]
+        public ActionResult Login(string returnUrl)
+        {
+            var model = new LoginModel
+            {
+                ReturnUrl = returnUrl
+            };
+
+            return View(model);
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public ActionResult Login(LoginModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            var user = userManager.Find(model.Email, model.Password);
+
+            if (user != null)
+            {
+                var identity = userManager.CreateIdentity(user, DefaultAuthenticationTypes.ApplicationCookie); // Creates Identity object from whatever user is returned from the DB.polymorphism?
+                GetAuthenticationManager().SignIn(identity);
+                return Redirect(GetRedirectUrl(model.ReturnUrl));
+            }
+
+     
+             // user authN failed
+             ModelState.AddModelError("", "Invalid email or password");
+             return View();
+        }
+
+        private IAuthenticationManager GetAuthenticationManager()
+        {
+            var ctx = Request.GetOwinContext();
+            return ctx.Authentication;
+        }
+        private string GetRedirectUrl(string returnUrl)
+        {
+            if (string.IsNullOrEmpty(returnUrl) || !Url.IsLocalUrl(returnUrl))
+            {
+                return Url.Action("Index", "Home");
+            }
+
+            return returnUrl;
+        }
+
+        public ActionResult LogOut()
+        {
+            var ctx = Request.GetOwinContext();
+            var authManager = ctx.Authentication;
+
+            authManager.SignOut("ApplicationCookie");
+            return RedirectToAction("index", "home");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                userManager.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        [AllowAnonymous]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult Register(RegisterViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            var user = new Member
+            {
+                UserName = model.Email,
+                //Nickname = model.Nickname
+            };
+
+            var result = userManager.Create(user, model.Password);
+
+            if (result.Succeeded) //UGH. Not done. Finish converting this. Slooooooow down. 
+            {
+                SignIn(user);
+                return RedirectToAction("Index", "Home");
+            }
+
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("", error);
+            }
+
+            return View();
+        }
+
+        [AllowAnonymous]
+        private void SignIn(Member user)
+        {
+
+            var identity = userManager.CreateIdentity(
+                user, DefaultAuthenticationTypes.ApplicationCookie);
+           
+            GetAuthenticationManager().SignIn(identity);
+        }
+
+    }
+}
